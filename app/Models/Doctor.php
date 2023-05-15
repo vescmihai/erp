950c5731c2f14762ea76c696d4cd3403e080fbc6
RapuJeanette <129312358+RapuJeanette@users.noreply.github.com>
2023-05-14 22:19:20 -0400

    Doctor.php

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -8,4 +8,33 @@ use Illuminate\Database\Eloquent\Model;
 class Doctor extends Model
 {
     use HasFactory;
+    
+    protected $fillable = [
+        'formacion',
+        'cargo',
+        'idEspecialidad',
+        'idSala',
+    ];
+
+    public function especialidad()
+    {
+        return $this->hasOne('App\Models\Especialidad', 'id', 'idEspecialidad');
+    }
+
+    public function sala()
+    {
+        return $this->hasOne('App\Models\Sala', 'id', 'idSala');
+    }
+
+    public function agenda(){
+        return $this->hasMany('App\Models\Agenda','idDoctor','id');
+    }
+
+    public function cita(){
+        return $this->hasMany('App\Models\Cita','idDoctor','id');
+    }
+
+    public function consulta(){
+        return $this->hasMany('App\Models\Consulta','idDoctor','id');
+    }
 }
